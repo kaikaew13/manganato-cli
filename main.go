02ecada51d0906c7a20f5f2843d85e7acbccf5dd
kaikaew13 <ksaratunti@gmail.com>
2021-07-10 20:27:55 +0700

    got initial gocui code in to make program quit when Crtl+C

--- main.go
+++ main.go
@@ -12,4 +12,19 @@ func main() {
 		log.Panicln(err)
 	}
 	defer g.Close()
+
+	g.Highlight = true
+	g.SelFgColor = gocui.ColorYellow
+	g.BgColor = gocui.ColorBlack
+	g.FgColor = gocui.ColorWhite
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		return gocui.ErrQuit
+	}); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
 }
