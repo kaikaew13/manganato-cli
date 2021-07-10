8aa0c3468720c9bb0439853c34e316180a36e0da
kaikaew13 <ksaratunti@gmail.com>
2021-07-10 20:19:45 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kaikaew13/manganato-cli
+
+go 1.16
+
+require (
+	github.com/jroimartin/gocui v0.4.0
+	github.com/nsf/termbox-go v1.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+}
